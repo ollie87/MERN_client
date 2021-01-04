00a7af6115ae4a15935361c7a2e757fd8a669c2b
mgarridov <mgarridov@minsait.com>
2021-01-04 13:37:23 +0100

    feat(NuevoProyecto): state added

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,12 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 const NuevoProyecto = () => {
+  const [proyecto, guardarProyecto] = useState({ nombre: '' });
+
+  const { nombre } = proyecto;
+
+  const onChangeProyecto = e => {
+    guardarProyecto({ ...proyecto, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitProyecto = e => {
+    e.preventDefault();
+  };
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         Nuevo Proyecto
       </button>
-      <form className="formulario-nuevo-proyecto">
-        <input type="text" className="input-text" placeholder="Nombre Proyecto" name="nombre" />
+      <form className="formulario-nuevo-proyecto" onSubmit={onSubmitProyecto}>
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Nombre Proyecto"
+          name="nombre"
+          onChange={onChangeProyecto}
+          value={nombre}
+        />
         <input type="submit" className="btn btn-primario btn-block" value="Agregar Proyecto" />
       </form>
     </Fragment>
