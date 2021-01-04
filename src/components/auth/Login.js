92c406131ed8661a11bb399a050b82250b822668
mgarridov <mgarridov@minsait.com>
2021-01-04 13:03:37 +0100

    feat(Login): local state added

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Login = () => {
-  const onChange = () => {};
+  const [usuario, guardarUsuario] = useState({ email: '', password: '' });
+  const { email, password } = usuario;
+  const onChange = e => {
+    guardarUsuario(...usuario, { [e.target.name]: e.target.value });
+  };
+  const onSubmit = e => {
+    e.preventDefault();
+  };
   return (
     <div className="form-usuario">
       <div className="contenedor-form sombra-dark">
         <h1>Iniciar Sesion</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="campo-form">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Tu Email" onChange={onChange} />
+            <input type="email" id="email" name="email" placeholder="Tu Email" value={email} onChange={onChange} />
           </div>
           <div className="campo-form">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" placeholder="Tu Password" onChange={onChange} />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              value={password}
+              onChange={onChange}
+            />
           </div>
           <div className="campo-form">
             <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión" />
